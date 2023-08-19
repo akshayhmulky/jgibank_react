4fce9babbaa449559222c55614fb67561c61c1cf
akshayhmulky <sfdc30443@gmail.com>
2023-08-19 21:49:10 +0530

    updated api

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,51 +1,23 @@
-// export const REGISTER_API = 'http://localhost:8080/api/v1/register';
-// export const LOGIN_API = 'http://localhost:8080/api/v1/login';
-// export const FETCH_CUSTOMER_BY_USERNAME_API =
-//   'http://localhost:8080/api/v1/customer/';
-// export const UPDATE_CUSOTOMER_PROFILE_BY_USERNAME_API =
-//   'http://localhost:8080/api/v1/customer/update';
-// export const GET_ACCOUNT_BY_USERNAME = 'http://localhost:8080/api/v1/accounts/';
-// export const ADD_ACCOUNT_BALANCE_BY_ACCOUNTNUMER =
-//   'http://localhost:8080/api/v1/account/addbalance';
-
-// export const ADD_BENEFICIARY = 'http://localhost:8080/api/v1/beneficiary/add';
-
-// export const FUND_TRANSFER =
-//   'http://localhost:8080/api/v1/account/fundtransfer';
-
-// export const ACCOUNT_OPENING = 'http://localhost:8080/api/v1/account/add';
-
-// export const GET_ALL_CUSTOMERS = 'http://localhost:8080/api/v1/customers';
-
-// export const CHANGE_USER_ROLE =
-//   'http://localhost:8080/api/v1/customer/rolechange';
-
-// =============================================PRODUCTION API======================================
-
-export const REGISTER_API =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/register';
-export const LOGIN_API =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/login';
+export const REGISTER_API = 'http://localhost:8080/api/v1/register';
+export const LOGIN_API = 'http://localhost:8080/api/v1/login';
 export const FETCH_CUSTOMER_BY_USERNAME_API =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/customer/';
+  'http://localhost:8080/api/v1/customer/';
 export const UPDATE_CUSOTOMER_PROFILE_BY_USERNAME_API =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/customer/update';
-export const GET_ACCOUNT_BY_USERNAME =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/accounts/';
+  'http://localhost:8080/api/v1/customer/update';
+export const GET_ACCOUNT_BY_USERNAME = 'http://localhost:8080/api/v1/accounts/';
 export const ADD_ACCOUNT_BALANCE_BY_ACCOUNTNUMER =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/account/addbalance';
+  'http://localhost:8080/api/v1/account/addbalance';
 
-export const ADD_BENEFICIARY =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/beneficiary/add';
+export const ADD_BENEFICIARY = 'http://localhost:8080/api/v1/beneficiary/add';
 
 export const FUND_TRANSFER =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/account/fundtransfer';
+  'http://localhost:8080/api/v1/account/fundtransfer';
 
-export const ACCOUNT_OPENING =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/account/add';
+export const ACCOUNT_OPENING = 'http://localhost:8080/api/v1/account/add';
 
-export const GET_ALL_CUSTOMERS =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/customers';
+export const GET_ALL_CUSTOMERS = 'http://localhost:8080/api/v1/customers';
 
 export const CHANGE_USER_ROLE =
-  'http://jgibank-env.eba-fut44mhc.us-east-1.elasticbeanstalk.com/api/v1/customer/rolechange';
+  'http://localhost:8080/api/v1/customer/rolechange';
+
+// =============================================PRODUCTION API======================================
